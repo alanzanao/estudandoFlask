4405e247d39dd065d82f92c5f24b82d50809b685
alanzanao <alan.zanao@outlook.com.br>
2020-07-25 17:38:50 -0300

    adicionado um database basico

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////testing.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+class Products(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(140))
+    price=db.Column(db.Numeric())
+    description=db.Column(db.Text)
+
+@app.cli.command()
+def createdb():
+    db.create_all()
 
 @app.route('/')
+def inicio():
     return "Olá visitante"
 
-@app.route('/product')
+@app.route('/products')
 def index():
-    products = {'name':'pão de sal', 'price':'0.5', 'description':'Very good'}
+    products=Products.query.all()
     return render_template("index.html", products=products)
 
-@app.route("/product/<product_id>")
+@app.route('/products/<product_id>')
 def product(product_id):
-    products = {'name':'pão de sal', 'price':'0.5', 'description':'Very good'}
-    return render_template("product.html", products=products)
-     
+    product=Products.query.filter_by(id=product_id).first() or abort(404, 'Produto não encontrado')
+    return render_template("product.html", product=product)
